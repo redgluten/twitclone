9bddd22aa679f7ad5ba9ebdf65ff2270a57ed0a0
gluten <goulvenschaal@me.com>
2017-10-07 18:14:28 +0200

    Updating views for new layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,29 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Plateforme de microblogging">
+@extends('layouts.public')
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen" title="no title">
+@section('title')
+Accueil
+@stop
 
-    <title>Twitter</title>
-</head>
-<body>
+@section('content')
     <div class="container">
         <div class="row">
             <h1>Dernier tweets</h1>
 
             <div class="col-md-6">
+
                 @foreach($tweets as $tweet)
                     <div class="media well">
-
                         <a class="media-left" href="#">
                             <!-- Photo de profil -->
                         </a>
-
                         <div class="media-body">
-                            <h5 class="media-heading">@??<small class="pull-right text-muted">{{ $tweet->created_at->diffForHumans() }}</small></h5>
-
+                            <h4 class="media-heading">@??<small class="pull-right text-muted">{{ $tweet->created_at->diffForHumans() }}</small></h4>
                             {{ $tweet->content }}
                         </div>
-
                     </div>
                 @endforeach
+
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -1,5 +1,22 @@
-<form action="" method="POST">
-    <input type="text" name="content">
+@extends('layouts.public')
 
-    <input type="submit" name="submit-tweet">
-</form>
+@section('title')
+Publier un nouveau tweet
+@stop
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <label for="content">Votre tweet</label>
+                        <textarea name="content" id="content" cols="30" rows="5" class="form-control"></textarea>
+                    </div>
+
+                    <input type="submit" class="btn btn-primary">
+                </form>
+            </div>
+        </div>
+    </div>
+@stop
