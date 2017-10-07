0f0848389420a5ddda633c27d24562c22b0d1485
gluten <goulvenschaal@me.com>
2017-10-07 18:23:24 +0200

    Updating the tweets form

--- resources/views/tweets/create.blade.php
+++ resources/views/tweets/create.blade.php
@@ -8,7 +8,9 @@ Publier un nouveau tweet
     <div class="container">
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
-                <form action="" method="POST">
+                <form action="{{ url('tweets') }}" method="POST">
+                    {{ csrf_field() }}
+
                     <div class="form-group">
                         <label for="content">Votre tweet</label>
                         <textarea name="content" id="content" cols="30" rows="5" class="form-control"></textarea>
