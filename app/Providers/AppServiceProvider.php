2c35fdc3096ff178f3b1d137502cf3ba16d1d56a
gluten <goulvenschaal@me.com>
2017-10-07 17:49:51 +0200

    Fix namespacing issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Set Carbon and PHP locale to the current app locale
-        Carbon\Carbon::setLocale(config('app.locale'));
+        \Carbon\Carbon::setLocale(config('app.locale'));
         setlocale(LC_TIME, 'fr_FR.utf8');
     }
 
