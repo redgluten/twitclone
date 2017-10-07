39cce4b6bd1120abaf664774df40e59853792d10
gluten <goulvenschaal@me.com>
2017-10-07 18:12:35 +0200

    Creating a blade layout

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Plateforme de microblogging">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen" title="no title">
+
+    <title>Twitter | @yield('title')</title>
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+</html>
