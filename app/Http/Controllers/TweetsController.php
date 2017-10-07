bc804ad6b51acc7edfaf631f957990547e7f651c
gluten <goulvenschaal@me.com>
2017-10-07 18:53:54 +0200

    Edit workflow with edit and update

--- app/Http/Controllers/TweetsController.php
+++ app/Http/Controllers/TweetsController.php
@@ -44,4 +44,35 @@ class TweetsController extends Controller
 
         return redirect('/');
     }
+
+    /**
+     * Edit the given resource
+     * @param  integer $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $tweet = Tweet::findOrFail($id);
+
+        return view('tweets.edit', compact('tweet'));
+    }
+
+    /**
+     * Update the given resource to storage
+     * @param  Request $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, ['content' => 'required|string|max:140']);
+
+        $tweet = Tweet::findOrFail($id);
+
+        $tweet->content = $request->content;
+
+        $tweet->save();
+
+        return redirect('/');
+    }
 }
